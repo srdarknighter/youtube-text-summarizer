a492d3f6650fa7f1a2f42e8462d7b4321c20965c
srdarknighter <145354061+srdarknighter@users.noreply.github.com>
2024-12-25 13:31:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,13 @@
 import streamlit as st
 import torch
+import transformers
+import youtube_transcript_api
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, pipeline
 import gdown
 
 @st.cache_resource
 def load_model():
-  url = 'https://drive.google.com/drive/folders/1-3OecGuYhc8G96e3hEnqNZNEycHPapLF'
+  url = 'https://drive.google.com/file/d/1-5IVbAXkeY63tdHZrsn4GIj5i8dp9juO/view?usp=drive_link'
   output = 'model_weights.pth'
   gdown.download(url, output, quiet=False)
   tokenizer = AutoTokenizer.from_pretrained("google/pegasus-cnn_dailymail")
@@ -21,7 +23,7 @@ from youtube_transcript_api import YouTubeTranscriptApi
 video_id = st.text_input("Enter the Youtube URL: ")
 video_id = video_id.split("=")[1]
 transcript1 = YouTubeTranscriptApi.get_transcript(video_id, languages=['en'])
-transcript = " ".join([t['text'] for t in transcript])
+transcript = " ".join([t['text'] for t in transcript1])
 
 if video_id and st.button("Summarize"):
   pipe = pipeline('summarization', model = model, tokenizer = tokenizer)
